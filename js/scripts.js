7e071b07445c6537f0234066c80fbb3786e2034f
Isaac Shoman and Ted Leary <ted@>
2018-03-21 11:49:50 -0700

    Add alert and JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-// Back-End Logic
-
-
-
-
 
 // Front-End Logic
+$(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    var name = $("input#name").val();
+    var street = $("input#street").val();
+    var state = $("input#state").val();
+    var zip = $("input#zip").val();
+    var reason = $("input#reason").val();
+    var date = $("input#date").val();
+    var time = $("input#time").val();
+    alert("Booking Successful!");
+    event.preventDefault();
+    });
+});
